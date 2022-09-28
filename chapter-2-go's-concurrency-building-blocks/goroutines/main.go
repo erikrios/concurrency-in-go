483b7cde22b45c953330ac601698882c5adc03b7
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-09-28 09:24:13 +0700

    Add the example code of chapter 2

---
+++ chapter-2-go's-concurrency-building-blocks/goroutines/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	for _, salutation := range []string{"hello", "greetings", "good day"} {
+		wg.Add(1)
+		go func(salutation string) {
+			defer wg.Done()
+			fmt.Println(salutation)
+		}(salutation)
+	}
+	wg.Wait()
+
+	memConsumed := func() uint64 {
+		runtime.GC()
+		var s runtime.MemStats
+		runtime.ReadMemStats(&s)
+		return s.Sys
+	}
+
+	var c <-chan interface{}
+	wg = sync.WaitGroup{}
+	noop := func() { wg.Done(); <-c }
+
+	const numGoroutines = 1e4
+	wg.Add(numGoroutines)
+	before := memConsumed()
+	for i := numGoroutines; i > 0; i-- {
+		go noop()
+	}
+
+	wg.Wait()
+	after := memConsumed()
+	fmt.Printf("%.3fkb", float64(after-before)/numGoroutines/1000)
+}
